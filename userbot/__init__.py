9a6b467845ecb5c17a12500de10a043fc64ec492
lunatic0der <83530725+lunatic0der@users.noreply.github.com>
2021-11-18 13:16:23 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -155,7 +155,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL", "https://github.com/mrismanaziz/Man-Userbot.git"
+    "UPSTREAM_REPO_URL", "https://github.com/lunatic0der/Man-Userbot.git"
 )
 UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Man-Userbot")
 
