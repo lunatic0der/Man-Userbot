ea97e84f8a30bce6b812b6f3b49bafe7cd935326
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-24 19:07:37 +0700

    Update costum.py

--- userbot/modules/costum.py
+++ userbot/modules/costum.py
@@ -9,114 +9,120 @@
 #
 """ Userbot module containing commands for keeping costum global notes. """
 
-from userbot import BOTLOG_CHATID
+from telethon import events
+
+from userbot.modules.sql_helper import snip_sql as sq
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, bot
-from userbot.events import man_cmd, register
+from userbot import BOTLOG_CHATID, CMD_HELP
+from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
 
-@register(outgoing=True, pattern=r"\.\w*", ignore_unsafe=True, disable_errors=True)
-async def on_snip(event):
-    """costums logic."""
+@man_cmd(pattern=r'\#(\S+)')
+async def incom_note(event):
+    if not BOTLOG_CHATID:
+        return
     try:
-        from userbot.modules.sql_helper.snips_sql import get_snip
+        if not (await event.get_sender()).bot:
+            notename = event.text[1:]
+            notename = notename.lower()
+            note = sq.get_note(notename)
+            message_id_to_reply = await reply_id(event)
+            if note:
+                if note.f_mesg_id:
+                    msg_o = await event.client.get_messages(
+                        entity=BOTLOG_CHATID, ids=int(note.f_mesg_id)
+                    )
+                    await event.delete()
+                    await event.client.send_message(
+                        event.chat_id,
+                        msg_o,
+                        reply_to=message_id_to_reply,
+                        link_preview=False,
+                    )
+                elif note.reply:
+                    await event.delete()
+                    await event.client.send_message(
+                        event.chat_id,
+                        note.reply,
+                        reply_to=message_id_to_reply,
+                        link_preview=False,
+                    )
     except AttributeError:
-        return
-    name = event.text[1:]
-    snip = get_snip(name)
-    message_id_to_reply = event.message.reply_to_msg_id
-    if not message_id_to_reply:
-        message_id_to_reply = None
-    if snip:
-        if snip.f_mesg_id:
-            msg_o = await event.client.get_messages(
-                entity=BOTLOG_CHATID, ids=int(snip.f_mesg_id)
-            )
-            await event.client.send_message(
-                event.chat_id,
-                msg_o.message,
-                reply_to=message_id_to_reply,
-                file=msg_o.media,
-            )
-            await event.delete()
-        elif snip.reply:
-            await event.client.send_message(
-                event.chat_id, snip.reply, reply_to=message_id_to_reply
-            )
-            await event.delete()
+        pass
 
-
-@bot.on(man_cmd(outgoing=True, pattern=r"costum (\w*)"))
-async def on_snip_save(event):
-    """For .costum command, saves costums for future use."""
-    try:
-        from userbot.modules.sql_helper.snips_sql import add_snip
-    except AtrributeError:
-        await event.edit("**Berjalan pada mode Non-SQL!**")
-        return
-    keyword = event.pattern_match.group(1)
-    string = event.text.partition(keyword)[2]
-    msg = await event.get_reply_message()
-    msg_id = None
-    if msg and msg.media and not string:
-        if BOTLOG_CHATID:
-            await event.client.send_message(
-                BOTLOG_CHATID,
-                f"📝 **#COSTUM**\
-            \n • **KEYWORD:** `{keyword}`\
-            \n • 🔖 Pesan ini disimpan sebagai catatan data untuk costum, Tolong JANGAN Dihapus!!",
-            )
-            msg_o = await event.client.forward_messages(
-                entity=BOTLOG_CHATID, messages=msg, from_peer=event.chat_id, silent=True
-            )
-            msg_id = msg_o.id
+@man_cmd(pattern="snip(?:\s|$)([\s\S]*)")
+async def add_snip(event):
+    if not BOTLOG_CHATID:
+        return await edit_delete(event, "You need to setup  `LOGGER_ID`  to save snips...")
+    trigger = event.pattern_match.group(1)
+    stri = event.text.partition(trigger)[2]
+    cht = await event.get_reply_message()
+    cht_id = None
+    trigger = trigger.lower()
+    if cht and not stri:
+        await event.client.send_message(BOTLOG_CHATID, f"#NOTE \n\nAdded Note with  `#{trigger}`. Below message is the output. \n**DO NOT DELETE IT**")
+        cht_o = await event.client.forward_messages(
+            entity=BOTLOG_CHATID, messages=cht, from_peer=event.chat_id, silent=True
+        )
+        cht_id = cht_o.id
+    elif cht:
+        return await edit_delete(event, f"**ERROR** \nReply to a message with `{hl}snip <trigger>` to add snips...")
+    if not cht:
+        if stri:
+            await event.client.send_message(BOTLOG_CHATID, f"#NOTE \n\nAdded Note with  `#{trigger}`. Below message is the output. \n**DO NOT DELETE IT**")
+            cht_o = await event.client.send_message(BOTLOG_CHATID, stri)
+            cht_id = cht_o.id
+            stri = None
         else:
-            await event.edit(
-                "**Menyimpan kostum dengan media membutuhkan `BOTLOG_CHATID` untuk disetel.**"
+            return await edit_delete(event, f"Invalid Syntax. Check  `{hl}plinfo snips`  to get proper Syntax.")
+    success = "**Successfully {} snip with trigger**  `#{}` "
+    if sq.add_note(trigger, stri, cht_id) is False:
+        sq.rm_note(trigger)
+        if sq.add_note(trigger, stri, cht_id) is False:
+            return await edit_or_reply(
+                event, f"Error Adding Snip.."
             )
-            return
-    elif event.reply_to_msg_id and not string:
-        rep_msg = await event.get_reply_message()
-        string = rep_msg.text
-    if (
-        add_snip(keyword, string, msg_id) is False
-        or add_snip(keyword, string, msg_id) is not False
-    ):
-        success = "**Costum {} disimpan. Gunakan** `.{}` **di mana saja untuk menggunakannya**"
-        await event.edit(success.format("Berhasil", keyword))
+        return await edit_or_reply(event, success.format("updated", trigger))
+    return await edit_or_reply(event, success.format("added", trigger))
 
-
-@bot.on(man_cmd(outgoing=True, pattern="costums$"))
-async def on_snip_list(event):
-    """For .costums command, lists costums saved by you."""
+@man_cmd(pattern="rmsnip(?:\s|$)([\s\S]*)")
+async def _(event):
+    input_str = (event.pattern_match.group(1)).lower()
+    if not input_str:
+        return await edit_delete(e, "I need a snip name to remove...")
+    if input_str.startswith("#"):
+        input_str = input_str.replace("#", "")
     try:
-        from userbot.modules.sql_helper.snips_sql import get_snips
-    except AttributeError:
-        await event.edit("**Berjalan pada mode Non-SQL!**")
-        return
-
-    message = "**Tidak ada kostum yang tersedia saat ini.**"
-    all_snips = get_snips()
-    for a_snip in all_snips:
-        if message == "**Tidak ada kostum yang tersedia saat ini.**":
-            message = "**List Costum yang tersedia:**\n"
-        message += f"✣ `.{a_snip.snip}`\n"
-    await event.edit(message)
+        sq.rm_note(input_str)
+        await edit_or_reply(event, "Removed  `#{}`  from snips..".format(input_str))
+    except:
+        await edit_or_reply(event, "No snip saved with this trigger.")
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"delcostum (\w*)"))
-async def on_snip_delete(event):
-    """For .delcostum command, deletes a costum."""
-    try:
-        from userbot.modules.sql_helper.snips_sql import remove_snip
-    except AttributeError:
-        await event.edit("**Berjalan pada mode Non-SQL!**")
-        return
-    name = event.pattern_match.group(1)
-    if remove_snip(name) is True:
-        await event.edit(f"**Berhasil menghapus costum:** `{name}`")
+@man_cmd(pattern="listsnip$")
+async def lsnote(event):
+    all_snips = sq.get_notes()
+    OUT_STR = "Available Snips:\n"
+    if len(all_snips) > 0:
+        for a_snip in all_snips:
+            OUT_STR += f"👉 #{a_snip.keyword} \n"
     else:
-        await event.edit(f"**Tidak dapat menemukan costum:** `{name}`")
+        OUT_STR = f"No Snips. Start Saving using `{hl}snip`"
+    if len(OUT_STR) > 4000:
+        with io.BytesIO(str.encode(OUT_STR)) as out_file:
+            out_file.name = "snips.text"
+            await event.client.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption="Available Snips",
+                reply_to=event
+            )
+            await event.delete()
+    else:
+        await edit_or_reply(event, OUT_STR)
+
 
 
 CMD_HELP.update(
